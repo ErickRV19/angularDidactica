5de38c7f80c3b86d3fbfc7bbc974c3ccf73f3179
LoyoSteve <ing.mendez999@gmail.com>
2021-06-07 16:23:28 -0500

    Juego memorama funcionanding

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -5,9 +5,11 @@ import {
   transition,
   trigger
 } from "@angular/animations";
+import { hostViewClassName } from "@angular/compiler";
 import { Component, OnInit } from "@angular/core";
 
 export interface CardData {
+  id: number;
   imageId: string;
   state: "default" | "flipped" | "matched";
 }
@@ -33,9 +35,10 @@ export interface CardData {
       state(
         "matched",
         style({
-          visibility: "false",
-          transform: "scale(0.05)",
-          opacity: 0
+          //visibility: "none",
+          //transform: "scale(0.05)",
+          transform: "rotateY(180deg)",
+          display: 'none'
         })
       ),
       transition("default => flipped", [animate("400ms")]),
@@ -46,21 +49,86 @@ export interface CardData {
 })
 
 export class CardsComponent implements OnInit {
-    data: CardData = {
-      imageId: "pDGNBK9A0sk",
-      state: "default"
-    };
-  items = [1, 4, 3, 2, 3, 2, 4, 1];
+    data: CardData[] = [
+      {
+        id: 0,
+        imageId: "pDGNBK9A0sk",
+        state: "default"
+      },
+      {
+        id: 1,
+        imageId: "n3zZfjs2M3Q",
+        state: "default"
+      },
+      {
+        id: 2,
+        imageId: "ZbS5V3mFVSQ",
+        state: "default"
+      }
+    ]
+
+  items: CardData[] = [];
+  cardBefore = {
+    pos: 1,
+    imgId: ''
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 0; i < (this.data.length*2); i++) {
+      if(this.data[i]){
+        this.items.push(this.data[i]);
+      }else{
+        var parCard: CardData = {
+          id: i,
+          imageId: this.data[i-this.data.length].imageId,
+          state: "default"
+        }
+        this.items.push(parCard);
+      }
+    }
+    this.items.sort(()=> Math.random() - 0.5);
   }
-  cardClicked() {
-    if (this.data.state === "default") {
-      this.data.state = "flipped";
+
+  cardClicked(pos: number, card: CardData) {
+    if (this.items[pos].state === "default") {
+      this.items[pos].state = "flipped";
     } else {
-      this.data.state = "default";
+      this.items[pos].state = "default";
+    }
+
+    this.verificarMatch(pos, card)
+  }
+
+  verificarMatch(pos: number, card: CardData){
+    //verifica si ya guardaste algo antes
+    if(this.cardBefore.imgId === ''){
+      //si no hay nada guarda la carta
+      this.cardBefore.pos = pos;
+      this.cardBefore.imgId = card.imageId;
+      
+    }else if(this.cardBefore.pos === pos){
+      //selecciono la misma carta
+      console.log('selecciono la misma');
+    }else{
+      //si no se guardo nada antes
+      //verifica si las cartas coinciden con su imagen
+      if(this.cardBefore.imgId === card.imageId){
+        //cartas concuerdan
+        //elimina la carta actual que selecciono y la carta anterior
+        this.items[pos].state = "matched";
+        this.items[this.cardBefore.pos].state = "matched";
+      }else{
+        //si las cartas no concuerdan
+        //regresa las cartas a su estado normal
+        this.items[pos].state = "default";
+        this.items[this.cardBefore.pos].state = "default";
+        
+      }
+      //limpia la carta guardada
+      this.cardBefore.pos = 0
+      this.cardBefore.imgId = '';
     }
   }
 
